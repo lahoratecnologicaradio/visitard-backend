09d83ea0a168155df0f0e05c3dcf6ffd9fef960f
Pedro M. Lara <pedrolarapeguero@gmail.com>
2026-08-07 00:20:44 -0400

    fix: correct pool import in beaches routes

--- src/routes/beaches.routes.js
+++ src/routes/beaches.routes.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
-const { pool } = require('../config/db');
+const pool = require('../config/db');
 
 const router = express.Router();
 
